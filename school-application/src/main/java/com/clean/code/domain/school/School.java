636efc220c2dc2a6dd4cdb5afb2f3274c16428be
Marcello Andrade <marcellohfandrade@gmail.com>
2020-02-13 23:12:42 -0300

    config git ignore, travis, readme

--- school-application/src/main/java/com/clean/code/domain/school/School.java
+++ school-application/src/main/java/com/clean/code/domain/school/School.java
@@ -3,8 +3,10 @@ package com.clean.code.domain.school;
 import java.util.List;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @Builder
@@ -14,5 +16,20 @@ public class School {
 	private UUID id = UUID.randomUUID();
 	private String name;
 	private List<EnumLevel> level;
+	
+	@Getter
+	@AllArgsConstructor
+	enum EnumLevel {		
+		ENSINO_INFANTIL("Ensino Infantil"),
+		ENSINO_FUNDAMENTAL("Ensino Fundamental"),
+		ENSINO_MEDIO("Ensino Médio"),
+		ENSINO_SUPERIOR("Ensino Superior");
+		
+		private String name;
+		
+		public static void main(String[] args) {
+			System.out.println(EnumLevel.ENSINO_FUNDAMENTAL.name);
+		}
+	}
 
 }
